c543b5a62a55792158be53a3613902a873b64ba0
Ethan <z.ethan144@gmail.com>
2023-04-13 18:38:30 +0300

    fixed numpy not imported

--- energy_dif.py
+++ energy_dif.py
@@ -1,9 +1,10 @@
+import numpy as np
 from numpy import cos
 
 def energy_dif_pair(J: float, theta_new: float, theta_old: float, theta_neighbor: float):
     return -J * cos(theta_new - theta_neighbor) + J * cos(theta_old - theta_neighbor)
 
-def energy_dif(J: float, theta_new: float, theta_old: float, neighbors: numpy.array):
+def energy_dif(J: float, theta_new: float, theta_old: float, neighbors: np.array):
     sum = 0
     for neighbor in neighbors:
         sum += energy_dif_pair(J, theta_new, theta_old, neighbor)
