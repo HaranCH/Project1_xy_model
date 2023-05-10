6b816e0d0378ce7136ac00ce08e32b17bfd4de55
Ethan <z.ethan144@gmail.com>
2023-05-10 18:23:18 +0300

    metropolis fixed, plotted energy(T)

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 J = 1
 L = 64
-numIter = 10e6
 n_theta = 16
 theta_values = np.linspace(2*pi / n_theta, 2*pi, n_theta)
 
@@ -18,21 +17,21 @@ def InitSpins(L: int):
             S[i,j] = np.random.choice(theta_values)
     return S
 
-def MetropolisXY(S: np.array, beta: float, J: float, num_iters: int):
+def MetropolisXY(S: np.array, beta: float, J: float, numIter: int):
     L = len(S[:,1])
     Snew = copy.deepcopy(S)
 
-    while True:
-        i, j = np.randint(0,L-1,2)
-        theta_old = S[i,j]
+    for x in range(numIter):
+        i, j = np.random.randint(0,L-1,2)
+        theta_old = Snew[i,j]
         theta_new = np.random.choice(theta_values)
         
-        N = np.zeros(1,4) # Neighbors
+        N = np.zeros((4, 1)) # Neighbors
 
-        N[0] = S[i,(j+1)%L]
-        N[1] = S[(i-1)%L,j]
-        N[2] = S[i,(j-1)%L]
-        N[4] = S[(i+1)%L,j]
+        N[0] = Snew[i,(j+1)%L]
+        N[1] = Snew[(i-1)%L,j]
+        N[2] = Snew[i,(j-1)%L]
+        N[3] = Snew[(i+1)%L,j]
 
         energydif = energy_dif(J, theta_new, theta_old, N)
 
@@ -43,8 +42,10 @@ def MetropolisXY(S: np.array, beta: float, J: float, num_iters: int):
         else:
             prob = 1
 
-        if np.random.rand() <= prob:
+        val = np.random.rand()
+        if  val <= prob:
             Snew[i,j] = theta_new
+    return Snew
 
 def PlotXY(S: np.array):
     L = len(S[:,1])
@@ -64,8 +65,6 @@ def EnergyXY(S: np.array, J: float):
     S_Right = np.roll(S, 1, axis=(1,0))
     sum += (cos(S - S_Up))
     sum += cos(S - S_Right)
-    print (np.sum(sum))
-    print("this ends here")
     return (-J*np.sum(sum)) / len(S)**2
     
 def CvXY(Energy: np.array, Temperature: np.array):
@@ -75,7 +74,57 @@ def CvXY(Energy: np.array, Temperature: np.array):
     Temp_dif = (Temperature - Temp_shifted)[1:]
     return Energy_dif / Temp_dif
 
+def magXY(S: np.array):
+    sum_cos = np.sum(cos(S))**2
+    sum_sin = np.sum(sin(S))**2
+    return (1/(len(S)**4))*(sum_cos + sum_sin)
+
+def CorrXY(S: np.array):
+    sum = 0
+    Cr = np.zeros((len(S)//2,1))
+    for r in range(1, len(Cr)):
+        S_Up = np.roll(S, r, axis=(0,1))
+        S_Right = np.roll(S, r, axis=(1,0))
+        Cr[r] = np.sum(cos(S - S_Up)) + np.sum(cos(S - S_Right))
+        Cr[r] /= len(S)**2
+    return Cr
+
 S = InitSpins(L)
-print(EnergyXY(S,J))
-# print(S)
-PlotXY(S)
+S = MetropolisXY(S, beta=1/0.02, J=J, numIter=int(1e4))
+
+betaA = 1/0.02
+betaB = 1/2
+numTPoints = 20
+KTPoints = np.linspace(1/betaA, 1/betaB, numTPoints)
+numMetropolis = 20
+avg_C = np.zeros((numTPoints, 1))
+avg_M = np.zeros((numTPoints, 1))
+avg_E = np.zeros((numTPoints, 1))
+for i in range(numTPoints):
+    for j in range(numMetropolis):
+        beta = 1/KTPoints[i]
+        S = MetropolisXY(S, beta, J, numIter=int(1e4))
+        avg_E[i] += EnergyXY(S, J)
+        avg_M[i] += magXY(S)
+        # avg_C[i] += CvXY()
+    avg_E[i] /= numMetropolis
+    avg_M[i] /= numMetropolis
+    # avg_C[i] /= numMetropolis
+
+plt.subplot(1, 2, 1)
+plt.plot(KTPoints, avg_E)
+plt.subplot(1, 2, 2)
+plt.plot(KTPoints, avg_M)
+plt.show()
+
+# print(f'Energy: {EnergyXY(S, J)}')
+# print(f'Mag: {magXY(S)}')
+# S = MetropolisXY(S, Beta, J)
+# PlotXY(S)
+# Beta = 1/10
+# numIter = 10e4
+# for i in range(iters):
+#     MetropolisXY(S, Beta, J)
+
+
+# PlotXY(S)
---
+++ test.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print([0.1] < 0.2)
